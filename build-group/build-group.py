e5177fe2a468237aee79d52233f494518c3adca6
Aleksandra Fedorova <afedorova@redhat.com>
2021-03-12 17:41:29 +0100

    Add possibility to group builds for the rebuild
    All builds started before the first completion timestamp, can be
    triggered simulatenously. Once they are done, all builds triggered
    before the second completion time can be triggered at once. And so on.

--- build-group/build-group.py
+++ build-group/build-group.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
+from datetime import datetime as dt
 import argparse
 import os
 import koji
+from koji_cli.lib import watch_tasks
 import logging
 
 
@@ -14,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 def request_sidetag(basetag="eln-build"):
     """Create sidetag for the output of the rebuild process"""
-    
+
     sidetag = KOJI.createSideTag(basetag)["name"]
     logger.debug(f"Created sidetag {sidetag}")
     return sidetag
@@ -37,32 +39,75 @@ def create_rebuild_list(builds):
     """
 
     to_rebuild = []
-    
+
     for build in builds:
         build_data = KOJI.getBuild(build)
         if is_eln(build_data["package_name"]):
             to_rebuild.append(build_data)
         else:
-            logger.info(f'Skipping {build_data["nvr"]} as it is not in ELN')            
+            logger.info(f'Skipping {build_data["nvr"]} as it is not in ELN')
 
     logging.debug(to_rebuild)
 
     return to_rebuild
 
-def rebuild_list(builds, target, opts={"scratch": True, 'fail_fast': True}):
+def rebuild_by_group(groups, target, **kwargs):
 
-    tasks = []
-    for build in builds:
-        task_id = KOJI.build(
-            src=build['extra']['source']['original_url'],
-            target=target,
-            opts=opts,
-        )
-        tasks.append(task_id)
+    opts={"scratch": True, 'fail_fast': True}
+    
+    opts.update(kwargs)
+
+    
+    logging.info(f'Processing {len(rebuild_groups)} groups')
+
+    for group in groups:
+        tasks = []
+        for build in group:
+            task_id = KOJI.build(
+                src=build['extra']['source']['original_url'],
+                target=target,
+                opts=opts,
+            )
+            tasks.append(task_id)
+        rv = watch_tasks(session, [tasks], poll_interval=10)
 
     return tasks
 
 
+def group_rebuild_list(builds):
+    """Split build in groups which can be rebuilt in parallel.
+
+    Split timeline by completion timestamps.
+
+    All builds which start before the minimal completion time stamp can be
+    triggered simulataneously. Out of remaining builds, all builds which started
+    before the second completion time can be started at once. An so on.
+    """
+
+    logging.debug(len(builds))
+    completion_times = sorted([dt.fromtimestamp(build["completion_ts"]) for build in builds])
+    logging.debug(f'Completion Times: {completion_times}')
+
+    rebuild_groups = []
+
+    for time in completion_times:
+        rebuild_group = []
+        reminder = []
+        for build in builds:
+            if dt.fromtimestamp(build["start_ts"]) < time:
+                rebuild_group.append(build)
+            else:
+                reminder.append(build)
+        if rebuild_group:
+            builds = reminder
+            rebuild_groups.append(rebuild_group)
+
+    return rebuild_groups
+
+def show_groups(groups):
+    for group in groups:
+        nvrs = [build["nvr"] for build in group]
+        print(", ".join(nvrs))
 
 if __name__ == "__main__":
 
@@ -103,7 +148,7 @@ if __name__ == "__main__":
 
     logger.debug(args)
 
-#    args.input_tag = 'f35-build-side-38024'
+ #   args.input_tag = 'f35-build-side-38412'
     args.output_tag = 'eln-build-side-38508'
 
     if args.output_tag:
@@ -112,12 +157,16 @@ if __name__ == "__main__":
         output_tag = request_sidetag()
 
     if args.input_tag:
-        builds = KOJI.listTagged(args.input_tag)
+        logger.info(f'Processing {args.input_tag}')
+        builds = KOJI.listTagged(args.input_tag, latest=True)
     else:
         builds = args.build
 
     to_rebuild = create_rebuild_list(builds)
 
-    tasks = rebuild_list(to_rebuild, target=output_tag)
+    rebuild_groups = group_rebuild_list(to_rebuild)
 
-    print(tasks)
+    show_groups(rebuild_groups)
+    exit(0)
+    
+    rebuild_by_group(rebuild_groups, target=output_tag, scratch=True)
